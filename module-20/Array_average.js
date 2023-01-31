97dda2c8ddc05703327ae63eca9c93e7cae2152e
md muhaiminul <muhai4959@gmail.com>
2023-01-31 09:38:50 +0600

    module-20

--- module-20/Array_average.js
+++ module-20/Array_average.js
@@ -1,17 +1,3 @@
-// const numbers = [1, 2, 3, 4, 5];
-
-// let sum = 0;
-// // forEach function for Array 
-// numbers.forEach(make_avg);
-// function make_avg(arrValue) {
-//     sum += arrValue;
-// }
-// // average the sumed value 
-// let average = sum / numbers.length;
-// console.log(average);
-
-
-
 // alternate code 
 const numbers = [45, 4, 9, 16, 25];
 
---
+++ module-20/pizza.js
@@ -0,0 +1,8 @@
+const pizza = {
+  toppings: ["cheese", "sauce", "pepperoni"],
+  crust: "deep dish",
+  serves: 2,
+};
+
+const pepperoni = pizza.toppings[2];
+console.log(pepperoni);
