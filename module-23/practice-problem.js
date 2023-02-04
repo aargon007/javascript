3e78508c064b4bb89018e25a928412104aa5526c
md muhaiminul <muhai4959@gmail.com>
2023-02-04 19:03:03 +0600

    bestfriend array alternate solution

--- module-23/practice-problem.js
+++ module-23/practice-problem.js
@@ -51,6 +51,19 @@ function bestFriend(arr){
 const bestFriends = ["sajid", "kamal", "jubayer bin rased", "chinku"];
 console.log(bestFriend(bestFriends));
 
+//alternative solution
+function bestFriend(name){
+    let friend = name[0];
+    for (let i = 0; i<name.length; i++){
+        let element = name[i];
+        if(element < friend){
+            friend = element
+        }
+    }
+    return friend;
+}
+const nameList = ["abul", "kabula", "babul", "abdullah"];
+console.log(bestFriend(nameList));
 
 /* এইটা একটু ট্রিকি হতে পারে। একটা array এর মধ্যে অনেকগুলা সংখ্যা থাকবে। তোমার কাজ হচ্ছে সংখ্যা গুলা একটার পর একটা করে চেক করা। এবং সংখ্যা গুলা যদি পজিটিভ সংখ্যা হয়। অর্থাৎ শূন্য বা শূন্যের চাইতে বড় হয় তাহলে সেগুলাকে কোন একটা array এর মধ্যে রাখবে। আর যদি নেগেটিভ সংখ্যা হয়। তাহলে লুপটা স্টপ করে দিবে। এবং লুপ বন্ধ করার আগ পর্যন্ত যতগুলা পজিটিভ সংখ্যা পাওয়া গেছে। সেগুলা রিটার্ন করে দিবে।  */
 
